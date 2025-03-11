f3f7be11327da7bf6d9dd99b983162883d534f74
llRedXD <gh.o.neira@hotmail.com>
2025-03-11 17:00:14 -0300

    feat: Atualizar caminho base de acordo com o ambiente selecionado

--- deployC.py
+++ deployC.py
@@ -1,3 +1,10 @@
+from datetime import datetime
+import os
+import re
+import subprocess
+from unittest import result
+
+
 class Deploy:
     def __init__(self):
         self.caminho_dev = r"\\smydev\d$\inetpub\wwwroot\\"
@@ -6,11 +13,83 @@ class Deploy:
         self.caminho_prod_bkp = r"\\smyapp2\\d$\\bkp\\"
         self.caminho_temp = "./temp/"
         self.caminho_base = ""
+        self.caminho_bkp = ""
         self.os = ""
         self.language = ""
+        self.caminho_projeto = ""
+        self.projeto = ""
 
     def definir_caminho_base(self, ambiente):
         if ambiente == "Dev":
             self.caminho_base = self.caminho_dev
+            self.caminho_bkp = self.caminho_dev_bkp
         else:
             self.caminho_base = self.caminho_prod
+            self.caminho_bkp = self.caminho_prod_bkp
+
+    # Python
+    def get_name_project(self, path):
+        with open(path, "r") as file:
+            for line in file:
+                if 'os.environ.setdefault("DJANGO_SETTINGS_MODULE"' in line:
+                    match = re.search(
+                        r'os\.environ\.setdefault\("DJANGO_SETTINGS_MODULE",\s*"(.*?)"\)',
+                        line,
+                    )
+                    if match:
+                        settings_module = match.group(1)
+                        self.projeto = settings_module.split(".")[0]
+
+    def get_projeto_python(self):
+        manage_path = os.path.join(self.caminho_projeto, "manage.py")
+        self.get_name_project(manage_path)
+        self.caminho_projeto = os.path.join(self.caminho_projeto, self.projeto)
+
+    # React
+    def get_projeto_react(self):
+        build_path = os.path.join(self.caminho_projeto, "build")
+        print(self.caminho_projeto)
+        if not os.path.exists(build_path):
+            subprocess.run(["powershell", "cd", self.caminho_projeto])
+            subprocess.run(
+                [("wsl" if self.os == "Linux" else ""), "npm", "run", "build"]
+            )
+        self.caminho_projeto = build_path
+
+    def criar_backup(self):
+        print("Criando backup")
+        if self.language == "Python":
+            self.get_projeto_python()
+        if self.language == "React":
+            self.get_projeto_react()
+        subprocess.run(
+            [
+                "powershell",
+                "cp",
+                "-r",
+                self.caminho_projeto,
+                self.caminho_bkp
+                + self.caminho_base.split("\\")[-1]
+                + f"_{datetime.now().strftime('%Y%m%d%H%M%S')}",
+            ],
+        )
+
+    def deploy(self):
+        print(self.language)
+        self.criar_backup()
+
+
+if __name__ == "__main__":
+    deploy = Deploy()
+    deploy.definir_caminho_base("Dev")
+    deploy.language = "React"
+    deploy.os = "Linux"
+    deploy.caminho_projeto = (
+        "\\\\wsl.localhost/Ubuntu-22.04/home/red/workspace/smy_intranet"
+    )
+    # deploy.caminho_projeto = (
+    #     "\\\\wsl.localhost/Ubuntu-22.04/home/red/workspace/back-end-django"
+    # )
+    # deploy.caminho_base = "\\\\smydev\d$\inetpub\wwwroot\SmyBackDjango"
+    deploy.caminho_base = "\\\\smydev\d$\inetpub\wwwroot\IntranetFrontend"
+    deploy.deploy()
--- menu.py
+++ menu.py
@@ -1,3 +1,4 @@
+import os
 import tkinter as tk
 from tkinter import filedialog
 
@@ -12,7 +13,7 @@ class Menu:
 
         # Configurações da janela
         self.root.title("Menu")
-        self.root.geometry("400x200")
+        self.root.geometry("600x400")
 
         self.environment_var = tk.StringVar()
         self.environment_var.set("Dev")  # Set default value to "Dev"
@@ -26,8 +27,11 @@ class Menu:
         self.language_var.set("Python")  # Set default value to "Python"
         self.language_options = ["Python", ".Net", "React"]
 
-        self.caminho_pasta = tk.StringVar()
-        self.caminho_pasta.set("")
+        self.caminho_projeto = tk.StringVar()
+        self.caminho_projeto.set("")
+
+        self.caminho_deploy = tk.StringVar()
+        self.caminho_deploy.set("")
 
         self.create_menu()
 
@@ -48,10 +52,30 @@ class Menu:
         )
         language_menu.pack()
 
-    def open_folder(self):
+    def open_folder_project(self):
+        self.deploy.os = self.os_var.get()
+        if self.deploy.os == "Windows":
+            desktop_path = os.path.join(
+                os.path.join(os.environ["USERPROFILE"]), "Desktop"
+            )
+            self.deploy.caminho_projeto = filedialog.askopenfilename(
+                initialdir=desktop_path, title="Selecione um arquivo"
+            )
+        else:
+            path_linux = "\\\\wsl.localhost/Ubuntu-22.04/home/"
+            # Check if there is only one folder inside the 'home' directory
+            folders = os.listdir(path_linux)
+            if len(folders) == 1:
+                path_linux = os.path.join(path_linux, folders[0])
+            self.deploy.caminho_projeto = filedialog.askdirectory(
+                initialdir=path_linux, title="Selecione uma pasta"
+            )
+
+        self.caminho_projeto.set(self.deploy.caminho_projeto)
+
+    def open_folder_deploy(self):
         # Alimentar o objeto Deploy com as informações selecionadas
         self.deploy.definir_caminho_base(self.environment_var.get())
-        self.deploy.os = self.os_var.get()
         self.deploy.language = self.language_var.get()
 
         # Consultar pasta do projeto.
@@ -60,7 +84,9 @@ class Menu:
             title="Selecione uma pasta",
         )
         # Faça algo com o caminho da pasta selecionada
-        self.caminho_pasta.set(folder_path)
+        self.caminho_deploy.set(folder_path)
+        if self.deploy.caminho_projeto:
+            self.button_deploy.config(state=tk.NORMAL)
 
     def create_menu(self):
         self.select_environment()
@@ -71,11 +97,29 @@ class Menu:
         label = tk.Label(self.root, text="Selecione uma pasta:")
         label.pack()
 
-        caminho_pasta_label = tk.Label(self.root, textvariable=self.caminho_pasta)
-        caminho_pasta_label.pack()
+        # Caminho projeto
+        caminho_projeto_label = tk.Label(self.root, textvariable=self.caminho_projeto)
+        caminho_projeto_label.pack()
 
-        button = tk.Button(self.root, text="Abrir pasta", command=self.open_folder)
-        button.pack()
+        button_projeto = tk.Button(
+            self.root, text="Abrir pasta", command=self.open_folder_project
+        )
+        button_projeto.pack()
+
+        # Caminho deploy
+        caminho_deploy_label = tk.Label(self.root, textvariable=self.caminho_deploy)
+        caminho_deploy_label.pack()
+
+        button_path_deploy = tk.Button(
+            self.root, text="Abrir pasta", command=self.open_folder_deploy
+        )
+        button_path_deploy.pack()
+
+        self.button_deploy = tk.Button(
+            self.root, text="Deploy", command=self.deploy.deploy
+        )
+        self.button_deploy.pack()
+        self.button_deploy.config(state=tk.DISABLED)
 
         self.root.mainloop()
 
